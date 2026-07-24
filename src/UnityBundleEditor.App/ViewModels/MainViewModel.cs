89f7ccf0c64775023c4b801d137ae0a8c87cfe63
Joskia <joskia@users.noreply.github.com>
2026-07-23 23:08:43 -0500

    fix: add AssetsTools.NET package reference and Microsoft.Maui.Storage using to App project

--- src/UnityBundleEditor.App/ViewModels/MainViewModel.cs
+++ src/UnityBundleEditor.App/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
 using AssetsTools.NET;
+using Microsoft.Maui.Storage;
 
 namespace UnityBundleEditor.App.ViewModels;
 
